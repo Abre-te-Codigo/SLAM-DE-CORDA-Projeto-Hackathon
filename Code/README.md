86304307fd540615ef622c483ae11b514dac9386
Jakeliny gracielly <jakeliny.dev@gmail.com>
2020-12-12 01:48:42 -0300

    add videos and code

---
+++ Code/README.md
@@ -0,0 +1,12 @@
+# slam-de-corda
+
+Projeto Slam de Corda com parceria com o Instituto de Estudo Brasileiros IEB submetido para o Hackathon Abra-te Código.
+Team: Equipe Sésamo
+
+Metadados:
+cordeis.json  - inclui os metadados de todos os cordeis, funciona com uma cópia local das imagens disponibilizadas em: https://drive.google.com/drive/folders/1Qd5GESToUqTA2UTV32vLbGxckdxVcGRx
+cordeis_cut.json - exemplo com poucas imagens no servidor, já que a coleção precisa tratamento para poder ser disponibilizada integramente
+
+Imagens:
+As imagens foram processadas com Image Magick oara criar uma coleção de formato homogêneo e tamanho reduzido.
+A versão funcional deste projeto se encontre em: http://chequesoto.info/slamdecorda/
---
+++ Code/main.js
@@ -0,0 +1,217 @@
+var cordeis = document.getElementById('catalogue');
+
+var mainID = Math.floor(Math.random() * Cordeis.length);
+
+function selector(a){
+  document.getElementById("consulta").style.visibility = 'hidden';
+  document.getElementById("cria").style.visibility = 'hidden';
+  switch (a) {
+    case 0:
+      document.getElementById("beat").pause();
+      break;
+    case 1:
+      document.getElementById("consulta").style.visibility = 'visible';
+      document.getElementById("beat").pause();
+      break;
+    case 2:
+      document.getElementById("cria").style.visibility = 'visible';
+      // document.getElementById("beat").play();
+      break;
+  }
+}
+
+function selectDisplay(){
+  document.getElementById('catalogue').value = mainID;
+  show(mainID);
+  if(document.getElementById("image").checked){
+    // document.getElementById("page").value = 1;
+    showPage();
+  }
+}
+
+function show(key){
+  // var id = document.getElementById("catalogue").value;
+  var id = key || mainID;
+  var pages = Cordeis[id].Folhas;
+  document.getElementById("titulo").innerHTML = Cordeis[id].Titulo;
+  document.getElementById("autor").innerHTML = Cordeis[id].Autor;
+  document.getElementById("data").innerHTML = Cordeis[id].Data;
+  document.getElementById("localidade").innerHTML = Cordeis[id].Localidade;
+  // document.getElementById("page").max = Cordeis[id].Folhas;
+  document.getElementById("totpag").innerHTML = Cordeis[id].Folhas;
+
+  // var p = 1;
+  // document.getElementById("page").value = 1;
+
+  document.getElementById("text_container").innerHTML = "";
+  document.getElementById("text_container").style.padding = "0 0 0 5%";
+  var textoHTML = Cordeis[id].Texto;
+  textoHTML = textoHTML.replace(/(\r\n|\n|\r)/gm,"<br>")
+  document.getElementById("text_container").innerHTML = textoHTML;
+
+}
+
+function showPage(){
+  var id = document.getElementById("catalogue").value;
+  document.getElementById("titulo").innerHTML = Cordeis[id].Titulo;
+  document.getElementById("autor").innerHTML = Cordeis[id].Autor;
+  document.getElementById("data").innerHTML = Cordeis[id].Data;
+  document.getElementById("localidade").innerHTML = Cordeis[id].Localidade;
+  // document.getElementById("page").max = Cordeis[id].Folhas;
+  document.getElementById("totpag").innerHTML = Cordeis[id].Folhas;
+  document.getElementById("text_container").style.padding = "0";
+  if (document.getElementById("image").checked){
+    var p = 1;
+    document.getElementById("text_container").innerHTML = "";
+
+    var img = new Image();
+    img.onload = function() {
+      // image exists and is loaded
+      // document.body.appendChild(img);
+      for (var k=1; k<=Cordeis[id].Folhas; k++){
+        document.getElementById("text_container").innerHTML +=
+        '<img src=\'cordeis_small/'+ Cordeis[id].Codigo + '-' + k + '.JPEG\' width=100% />';
+      }
+    }
+    img.onerror = function() {
+      // image did not load
+      // var err = new Image();
+      // err.src = 'cordeis_small/'+ Cordeis[id].Codigo + '-0' + p + '.JPEG';
+      // document.body.appendChild(err);
+      for (var k=1; k<=Cordeis[id].Folhas; k++){
+        if (k<10){
+          document.getElementById("text_container").innerHTML +=
+          '<img src=\'cordeis_small/'+ Cordeis[id].Codigo + '-0' + k + '.JPEG\' width=100% />';
+        } else {
+          document.getElementById("text_container").innerHTML +=
+          '<img src=\'cordeis_small/'+ Cordeis[id].Codigo + '-' + k + '.JPEG\' width=100% />';
+        }
+      }
+    }
+    img.src = 'cordeis_small/'+ Cordeis[id].Codigo + '-' + p + '.JPEG';
+  }
+}
+
+window.onload = function() {
+  var option;
+  for (var i = 0; i < Cordeis.length; i++) {
+      option = document.createElement('option');
+      option.text = Cordeis[i].Titulo;
+      option.value = i;
+      cordeis.add(option);
+  }
+  document.getElementById("catalogue").value = Math.floor(Math.random() * Cordeis.length);
+};
+
+
+function findCordel(sometext){
+  var result = [];
+  var amount = 60;
+  var key = 0;
+  var text = "";
+  var len = sometext.length;
+  for (var i = 0; i < Cordeis.length; i++){
+    if ( Cordeis[i].Titulo.search(sometext) > 0 ){
+      var start = Cordeis[i].Titulo.search(sometext);
+      key = i;
+      if (start >= amount/2){
+        text = "... " + Cordeis[i].Titulo.substring(start - amount/2,start) + "<b>" +
+          Cordeis[i].Titulo.substr(start,len) + "</b>" +
+          Cordeis[i].Titulo.substr(start+len, amount/2) + "...";
+      } else {
+        text = Cordeis[i].Titulo.substr(0,start) + "<b>" +
+          Cordeis[i].Titulo.substr(start,len) + "</b>" +
+          Cordeis[i].Titulo.substr(start+len,amount-start+len) + "... ";
+      }
+      result.push({key,text});
+    } else if (Cordeis[i]["Palavras-chave"].search(sometext) > 0) {
+      var start = Cordeis[i]["Palavras-chave"].search(sometext);
+      key = i;
+      text = "<b>" + Cordeis[i]["Palavras-chave"].substr(start,len) + "</b>" +
+        Cordeis[i]["Palavras-chave"].substr(start+len,amount-len) + "...";
+      result.push({key,text});
+    } else if (Cordeis[i].Texto.search(sometext) > 0) {
+      var start = Cordeis[i].Texto.search(sometext);
+      key = i;
+      if (start >= amount/2){
+        text = "..."+  Cordeis[i].Texto.substring(start - amount/2,start) + "<b>" +
+          Cordeis[i].Texto.substr(start,len) + "</b>" +
+          Cordeis[i].Texto.substr(start+len, amount/2) + "...";
+      } else {
+        text = Cordeis[i].Texto.substr(0,start) + "<b>" +
+          Cordeis[i].Texto.substr(start,len) + "</b>" +
+          Cordeis[i].Texto.substr(start+len,amount-start+len) + "...";
+      }
+      result.push({key,text});
+    }
+  }
+  return result;
+}
+
+function buscaCordel(texto){
+  document.getElementById("buscaMenu").innerHTML="";
+  var show = [];
+  var len = texto.length;
+  if (len >= 3) {
+    show = findCordel(texto);
+    if (show.length > 0){
+      for (var i=0; i<show.length; i++){
+        var node = document.createElement('div');
+        node.innerHTML = "<a href='#' onclick='inspira("+ show[i].key +");'>"
+        + show[i].text +"</a>";
+        document.getElementById("buscaMenu").appendChild(node);
+      }
+    } else {
+      var node = document.createElement('div');
+      node.innerHTML = "Ups!!! Essa busca não produz resultados, tenta de novo ;)";
+      document.getElementById("buscaMenu").appendChild(node);
+    }
+  } else if (len > 0) {
+    var node = document.createElement('div');
+    node.innerHTML = "Digite pelo menos 3 caracteres...<br/>+ Clique 2 vezes no resultado!";
+    document.getElementById("buscaMenu").appendChild(node);
+  }
+}
+
+function inspira(num) {
+  selector(2);
+  mainID = num;
+  var textoHTML = Cordeis[num].Texto;
+  textoHTML = textoHTML.replace(/(\r\n|\n|\r)/gm,"<br>")
+  document.getElementById("content2").innerHTML = textoHTML;
+  document.getElementById("criaInfoCordel").innerHTML =
+    Cordeis[num].Titulo + "<br/><br/><b> " +
+    Cordeis[num].Autor + "</b>";
+}
+
+function clearText(){
+  document.getElementById('areatexto').value = "";
+  updateTextFile();
+}
+
+var fileNameToSaveAs = 'rima-download.txt';
+var theLink = document.getElementById("downloadText");
+
+function updateTextFile() {
+  var text = document.getElementById('areatexto').value;
+  var theBlob = new Blob([text], {type: 'text/plain'});
+  localStorageToFile(theLink,theBlob);
+  return false;
+}
+
+function localStorageToFile(downloadLink,blob) {
+    downloadLink.download = fileNameToSaveAs;
+    if (window.URL !== null) {
+        // Chrome allows the link to be clicked without actually adding it to the DOM
+        downloadLink.href = window.URL.createObjectURL(blob);
+        downloadLink.target = `_blank`;
+    } else {
+        downloadLink.href = window.URL.createObjectURL(blob);
+        downloadLink.target = `_blank`;
+        downloadLink.style.display = 'none';
+        // add .download so works in Firefox desktop.
+        document.body.appendChild(downloadLink.download);
+    }
+    // downloadLink.click();
+    return false;
+}
